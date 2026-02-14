9f6598f09ea0e0642e24eb88293a68a00abedbe5
Suman Pun Magar <mannmagar19gmail.com>
2026-02-14 21:50:11 +0545

    day 8 inserting and removing elements ,class using the js

---
+++ JS/js14_Insertingandremovingelements/script.js
@@ -0,0 +1,37 @@
+/*
+
+*/
+document.querySelector(".box").innerHTML
+
+document.querySelector(".container").innerHTML
+
+document.querySelector(".container").innerText
+
+
+document.querySelector(".container").outerHTML //give the container itself with the inner html 
+
+
+document.querySelector(".container").tagName // for only elements
+
+document.querySelector(".container").nodeName // for all nodes including comment text, elements etc
+
+document.querySelector(".container").textContent
+document.querySelector(".box").textContent
+
+
+document.querySelector(".box").Hidden
+document.querySelector(".box").Hidden=true
+document.querySelector(".box").innerHTML="Hey i am a box "
+
+document.querySelector(".box").hasAttribute("style")
+document.querySelector(".box").getAttribute("style")
+document.querySelector(".box").setAttribute("style","display : inline")
+document.querySelector(".box").removeAttribute("style")
+document.querySelector(".box").attributes
+
+
+document.designMode="on" // enable editing any website
+
+document.querySelector(".box").dataset 
+
+
