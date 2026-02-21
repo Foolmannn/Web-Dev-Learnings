097e2cb0b7d7107161a258ee34970a169261551f
Suman Pun Magar <mannmagar19@gmail.com>
2026-02-21 21:52:52 +0545

    day 12 solved questions 4,5

--- JS/js21_QNAforJS/Solutions/04solution.js
+++ JS/js21_QNAforJS/Solutions/04solution.js
@@ -2,6 +2,18 @@
 // 4. The Password Validator:
 //    You are building a password validation feature. Create a function that checks if a given password meets the following criteria: at least 8 characters long, contains both uppercase and lowercase letters, and includes at least one digit.
 
+password ="sdfjksad"
+password2 ="Suman123"
 
 
-
+const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8}$/;
+function passcheck(pass){
+    if(passwordRegex.test(pass)){
+        console.log("valid password")
+    }
+    else{
+        console.log("try another password")
+    }
+}
+passcheck(password)
+passcheck(password2)
--- JS/js21_QNAforJS/Solutions/05solution.js
+++ JS/js21_QNAforJS/Solutions/05solution.js
@@ -1,3 +1,23 @@
 
 // 5. The Sum Selector:
 //    You are working on a function that should sum all numbers in an array until it encounters a negative number. Write a function that performs this summation.
+
+arr = [1,2,3,-4,5,6,-9]
+let s=0;
+function sum(a){
+    for (const key in a) {
+        if (!Object.hasOwn(a, key)) continue;
+        
+        const element = a[key];
+        if(element>0){
+            s+=element;
+        }
+        else{
+            s+=0;
+        }
+        
+        
+    }
+    return s;
+}
+console.log(sum(arr))
