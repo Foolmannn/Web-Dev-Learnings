1f08a246c7de10ad4e68b0bd03fa73f663f073d8
Suman Pun Magar <mannmagar19@gmail.com>
2026-02-21 21:38:34 +0545

    day 12 solved questions 1,2,3

--- JS/js21_QNAforJS/Solutions/02solution.js
+++ JS/js21_QNAforJS/Solutions/02solution.js
@@ -1,2 +1,20 @@
 // 2. The Double Trouble:
 //    You are tasked with writing a function that doubles each element in an array. However, there's a catch: if the array contains consecutive duplicate elements, only double one of them.
+
+let arr = [1,2,3,4,4,5,6,6,7,6,8,9];
+let arrSolution=[];
+x=0;
+function arrDoubler(a){
+    for (const e of arr) {
+        if(x==e){
+            arrSolution.push(e);
+        }
+        else{
+
+            arrSolution.push(e*2);
+        }
+        x=e;
+    }
+}
+arrDoubler(arr)
+console.log(arrSolution)
--- JS/js21_QNAforJS/Solutions/03solution.js
+++ JS/js21_QNAforJS/Solutions/03solution.js
@@ -1,2 +1,30 @@
 // 3. The Mirror Mirror:
 //    Imagine you have a string, and you need to create a new string that is a mirror image of the original. Write a function that appends the reversed version of the original string to itself.
+
+// let str ="Hello hi are you "
+
+// function stringmirror(s){
+//     let splitstr =str.
+//     console.log(splitstr);
+    
+// }
+let str2="";
+const str = "Hello";
+const chars= [...str];
+
+for (let index = chars.length -1  ; index >=0 ; index--) {
+    const element = chars[index];
+    // console.log(element)
+ str2 +=element;
+
+    // str2.concat(element);
+    
+}
+// for (const char of str) {
+//     // console.log(char); // Logs each character
+//     str2 =str+char
+
+// }   
+console.log(str+ str2);
+
+// stringmirror()
