029564497fe2675c92fb6bbc604a5cf91ecf9843
Suman Pun Magar <mannmagar19@gmail.com>
2026-02-21 21:54:37 +0545

    day 12 solved questions 4,5

--- JS/js21_QNAforJS/Solutions/05solution.js
+++ JS/js21_QNAforJS/Solutions/05solution.js
@@ -3,8 +3,9 @@
 //    You are working on a function that should sum all numbers in an array until it encounters a negative number. Write a function that performs this summation.
 
 arr = [1,2,3,-4,5,6,-9]
-let s=0;
+arr2 =[3,4,-6,-7,8]
 function sum(a){
+    let s=0;
     for (const key in a) {
         if (!Object.hasOwn(a, key)) continue;
         
@@ -21,3 +22,4 @@ function sum(a){
     return s;
 }
 console.log(sum(arr))
+console.log(sum(arr2))
