9607c14508b7274c4bb9ab0772cc18dfdd8e218d
Suman Pun Magar <mannmagar19@gmail.com>
2026-03-02 21:38:41 +0545

    day 15 learning nodejs and npm

---
+++ Backend-Learnings/server.js
@@ -0,0 +1,10 @@
+console.log("Hello World")
+// in terminal 
+// npm init 
+// this initialize the npm pacakge manager and create the package.json which can be used to add any packages
+// npm i slugify 
+// This installs the package and create the node modules folder and install the dependecies and add it to the packages .json
+
+// if the node modules get deleted then the 
+// npm install adds all the dependencies from the package.json file 
+
