6b6821e5dd22bd3e4cf126554131799282503cf2
Suman Pun Magar <mannmagar19@gmail.com>
2026-03-02 22:46:00 +0545

    day16 node js common vs ecma

--- backendlearnings/86commonjsVSEcmascript/main.js
+++ backendlearnings/86commonjsVSEcmascript/main.js
@@ -1,20 +1,44 @@
 
 
+// require is the used in the commonjs type of the projects in node js and common js in default
 
-const { createServer } = require('node:http');
-const fs = require("fs");
+// const { createServer } = require('node:http');
+// const fs = require("fs");
+
+// for the ecma script / type should be changed to the module in the package.json
+
+// import http from "http"
 
 
-const hostname = '127.0.0.1';
-const port = 3000;
-const server = createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/html'); // this header determines how output is showcased 
-  res.end('<hq>Hello World</h1>');
-});
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
+//curly braces for the named exports
+
+// import {a , b ,c} from "./mymodule.js"
+
+// console.log(a,b,c)
+
+// this works as the obj is default export so if no name is given it gives the default export 
+
+// import suman from "./mymodule.js"
+
+// console.log(suman) 
+
+
+// for the common js type 
+
+// we need to use the require\
+const a = require("./mymodule2.js")
+console.log(a)
+
+// const hostname = '127.0.0.1';
+// const port = 3000;
+// const server = createServer((req, res) => {
+//   res.statusCode = 200;
+//   res.setHeader('Content-Type', 'text/html'); // this header determines how output is showcased 
+//   res.end('<hq>Hello World</h1>');
+// });
+// server.listen(port, hostname, () => {
+//   console.log(`Server running at http://${hostname}:${port}/`);
+// });
 
 // to restart server each time while the code is altered 
 // is unnecessary
---
+++ backendlearnings/86commonjsVSEcmascript/mymodule2.js
@@ -0,0 +1,8 @@
+// module.exports ={
+//     a:1,
+//     b:6
+// }
+
+let c =56;
+
+module.exports =c;
