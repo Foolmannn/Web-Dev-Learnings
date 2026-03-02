08b79f31e95767daf2e379f169908e70dd1fe44c
Suman Pun Magar <mannmagar19@gmail.com>
2026-03-02 21:52:36 +0545

    day 15 setting basic nodejs server

--- Backend-Learnings/server.js
+++ Backend-Learnings/server.js
@@ -8,3 +8,15 @@ console.log("Hello World")
 // if the node modules get deleted then the 
 // npm install adds all the dependencies from the package.json file 
 
+// what slugify package does is it removes the space and alter the string so that they can be used as the url as url doesnot allow all the symbols and strings 
+
+var slugify = require('slugify')
+
+let a = slugify("some string") // some-string
+
+console.log(a)
+
+// if you prefer something other than '-' as separtor
+const b = slugify('some st&&(@@#@#>ring' , '_') // some_string
+console.log(b)
+
