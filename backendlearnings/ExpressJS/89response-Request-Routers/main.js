f3f4edc06fbff2d09ff0bbe1ab84895c92ee26d8
Suman Pun Magar <mannmagar19@gmail.com>
2026-03-05 21:22:26 +0545

    day 18 res.senfile and res.json and other respoonse methods

--- backendlearnings/ExpressJS/89response-Request-Routers/main.js
+++ backendlearnings/ExpressJS/89response-Request-Routers/main.js
@@ -6,10 +6,10 @@ const port = 3000;
 
 // get has the limit of the 8 KB and it is sent through the url
 
-app.use(express.static("public"));
+app.use(express.static("public")); // this is middleware which helps to share the file publically 
 
 app.get("/", (req, res) => {
-      console.log("Hey i am send using the get request ");
+  console.log("Hey i am send using the get request ");
   res.send("Hello World! using get request ");
 });
 
@@ -19,15 +19,28 @@ app.post("/", (req, res) => {
   res.send("Hello World send using the post method");
 });
 
+app
+  .put("/", (req, res) => {
+    console.log("Hey i am send using the put request ");
 
-app.put("/", (req, res) => {
-  console.log("Hey i am send using the put request ");
+    res.send("Hello World send using the put  method");
+  })
+  .get("/index", (req, res) => {
+    // in this way request can be chained in express
+    console.log("Hey its index");
 
-  res.send("Hello World send using the put  method");
+    //   res.send("This is index page "); instead of sending the this text we need to send the index.html file
+    // res.sendFile("templates/index.html"); this doesnot as express need the absolute path or the we need to specify the root path
+
+    // res.sendFile('templates/index.html',{root: __dirname}); // this is specifying the root as the current directory
+    res.sendFile(
+      "F:\\Web Dev Learnings\\backendlearnings\\ExpressJS\\89response-Request-Routers\\templates\\index.html",
+    ); // this and above will work
+  });
+
+app.get("/api", (req, res) => {
+  res.json({ a: 2, b: 3, c: 4, d: 5 , name:["harry" , "suman "]});
 });
-
-
-
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
