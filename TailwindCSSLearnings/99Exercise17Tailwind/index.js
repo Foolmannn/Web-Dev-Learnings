c9d166f45148c50bf7c0e01546f1c04678127fef
Suman Pun Magar <mannmagar19@gmail.com>
2026-03-11 22:04:07 +0545

    day 25 setup the express and created the endpoint for the html file

--- TailwindCSSLearnings/99Exercise17Tailwind/index.js
+++ TailwindCSSLearnings/99Exercise17Tailwind/index.js
@@ -2,12 +2,14 @@ import express from "express";
 const app = express()
 const port = 3000
 
+app.use(express.static("public"))
+
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
-// app.get('/home', (req, res) => {
-//   res.sendFile("F:\\Web Dev Learnings\\TailwindCSSLearnings\\99Exercise17Tailwind\\index.html")
-// })
+app.get('/home', (req, res) => {
+  res.sendFile("F:\\Web Dev Learnings\\TailwindCSSLearnings\\99Exercise17Tailwind\\public\\index.html")
+})
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
