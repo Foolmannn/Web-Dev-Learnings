5ccea51d8cff40433f24afb8505bc625ff566fd6
Suman Pun Magar <mannmagar19@gmail.com>
2026-03-13 22:12:48 +0545

    day 28 used the components and created them

--- REACTLearnings/105IntrotoReact/with-react/src/App.js
+++ REACTLearnings/105IntrotoReact/with-react/src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from "./logo.svg";
+import "./App.css";
+import { useState } from "react";
+import Navbar from "./components/Navbar"; // this import navbar.js
+import Footer from "./components/Footer";
+
 
 function App() {
+  const [value, setValue] = useState(0); // here value is vairable and setValue will be set by some event or logic
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* as class is reserved OOP of the js so className isused in react as react app uses the js to parse the html like looking code which we are currently using   */}
+      {/* In this return html code the js is parsed using the { } ie everthing inside the { } is parsed as the js values or variables  */}
+
+      <Navbar/> 
+      {/* <navbar/> ::::: Your problem happens because React components must start with a capital letter. Right now you imported and used navbar in lowercase, so React treats it like a normal HTML tag, not a component. */}
+      <div className="value">{value}</div>
+      <button
+        onClick={() => {
+          setValue(value + 1);
+        }}
+      >
+        Click Me
+      </button>
+      {/* <h1>Hello </h1> */}
+      <Footer/>
     </div>
   );
 }
---
+++ REACTLearnings/105IntrotoReact/with-react/src/components/Navbar.js
@@ -0,0 +1,19 @@
+    //rafce snippets from extension to quickly create the react arrow function component export
+    
+    
+    import React from 'react'
+    
+    const Navbar = () => {
+      return (
+        <div>
+          <ul>
+            <li>Home</li>
+            <li>About</li>
+            <li>Contact Us</li>
+          </ul>
+        </div>
+      )
+    }
+    
+    export default Navbar
+    
