f6e6a7255b88822fc983fd5870113bcf6e2587bc
Suman Pun Magar <mannmagar19@gmail.com>
2026-03-16 21:54:47 +0545

    day 31 example of the event handling

--- REACTLearnings/112EventsHandlingInReact/src/App.jsx
+++ REACTLearnings/112EventsHandlingInReact/src/App.jsx
@@ -6,6 +6,21 @@ import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
+  // const [name, setname] = useState("Suman")
+ // without this we cannot change the value  
+ const [form, setform] = useState({name:"", phone:""})
+// const handleChange = (e)=>{
+//   setname(e.target.value) // it takes the value entered in the form and update the state with that new value
+// }
+
+
+// handling multiple inputs with the single function 
+const handleChange = (e)=> {
+  setform({...form,[e.target.name]:e.target.value}) // this  means in the form state the name of the input is 
+  console.log(form) // this will console the incomplete input as the asynchronouse nature of the function serform 
+}
+
+
   const handleClick= ()=>{
     alert("I am clicked");
   }
@@ -22,6 +37,11 @@ function App() {
 <div className="red" onMouseOver={handlemouseover}>
   I am a red div . 
 </div>
+
+{/* <input type="text" value={name} onChange={handleChange}/>  */}
+{/* Wihout the onchange function we cannot enter the new value */}
+<input type="text" name='name' value={form.name} onChange={handleChange}/>
+<input type="text" name='phone' value={form.phone} onChange={handleChange}/>
 
   </>
   )
