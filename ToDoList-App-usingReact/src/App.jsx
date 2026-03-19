51f3a5e2a9e0ffe1965b6966853c07ea8e3f6ba1
Suman Pun Magar <mannmagar19@gmail.com>
2026-03-19 23:07:11 +0545

    Day 34  creating the basic structre and adding some css

--- ToDoList-App-usingReact/src/App.jsx
+++ ToDoList-App-usingReact/src/App.jsx
@@ -3,19 +3,29 @@ import { useState } from "react";
 import Navbar from "./components/Navbar";
 import "./index.css";
 
-
 export default function App() {
-  const [color, setColor] = useState('#ef4444'); // Starts as Red
+  const [color, setColor] = useState("#ef4444"); // Starts as Red
 
   return (
     <>
-    <Navbar/>
-<div className="hello bg-success  p-4  ">
-  Tailwind Working 
-</div>
-    <div className="bg text-white p-4">
-      Working Test
-    </div>
+        <Navbar />
+      <div className="main bg-violet-200  my-5 p-5 rounded-xl mx-auto min-h-[80vh] ">
+        <div className="addtodo text-black text-2xl ">
+          <h1>Add New Todo</h1>
+          <input type="text"className="bg-bg rounded "/>
+          <button>Add</button>
+        </div>
+    
+        <h2 className="bg text-black text-2xl ">Your Todos</h2>
+        <div className="todos">
+          <div className="todo flex gap-10 ">
+            <div className="text ">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Reiciendis, ad.</div>
+            <button className="edit bg-primary-dark  px-3 rounded-md hover:bg-primary  cursor-pointer  text-white ">Edit</button>
+            <button className="delete bg-primary-dark px-3 rounded-md hover:bg-primary cursor-pointer  text-white ">Delete</button>
+
+          </div>
+        </div>
+      </div>
     </>
   );
 }
--- ToDoList-App-usingReact/src/components/Navbar.jsx
+++ ToDoList-App-usingReact/src/components/Navbar.jsx
@@ -2,14 +2,14 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <div className='nav flex bg-amber-300 justify-between' >
+    <div className='nav flex bg-primary justify-between p-5 font-bold ' >
     <div className="logo">
       <span className='font-bold text-3xl mx-8 mt-8 '>PlanWhatYouDo</span>
     </div>
-    <ul className="flex list-none gap-10 mx-10 text-2xl">
+    <ul className="flex list-none gap-12 mx-10 text-2xl   ">
       
-        <li className='cursor-pointer hover:font-bold transition-all duration-75 '>Home</li>
-        <li className='cursor-pointer hover:font-bold transition-all duration-75 '>Tasks</li>
+        <li className='cursor-pointer hover:font-extrabold transition-all duration-75 '>Home</li>
+        <li className='cursor-pointer hover:font-extrabold transition-all duration-75 '>Tasks</li>
       
     </ul>
     </div>
