1cb84a9f316518a43076bd121693e6fcc995a868
Suman Pun Magar <mannmagar19@gmail.com>
2026-03-19 23:16:07 +0545

    Day 34  creating the basic structre and adding some css

--- ToDoList-App-usingReact/src/App.jsx
+++ ToDoList-App-usingReact/src/App.jsx
@@ -9,19 +9,19 @@ export default function App() {
   return (
     <>
         <Navbar />
-      <div className="main bg-violet-200  my-5 p-5 rounded-xl mx-auto min-h-[80vh] ">
-        <div className="addtodo text-black text-2xl ">
-          <h1>Add New Todo</h1>
-          <input type="text"className="bg-bg rounded "/>
-          <button>Add</button>
+      <div className="main bg-violet-200  mb-5 p-5 rounded-xl mx-auto min-h-[80vh] ">
+        <div className="addtodo my-5  text-black  ">
+          <h1 className="font-bold ">Add New Todo</h1>
+          <input type="text"className="w-2/3 bg-bg rounded "/>
+          <button className=" font-bold bg-primary-dark py-1  px-3 rounded-md hover:bg-primary cursor-pointer  text-white ml-5 " >Add</button>
         </div>
     
-        <h2 className="bg text-black text-2xl ">Your Todos</h2>
+        <h2 className="bg text-black font-bold text-2xl ">Your Todos</h2>
         <div className="todos">
           <div className="todo flex gap-10 ">
             <div className="text ">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Reiciendis, ad.</div>
-            <button className="edit bg-primary-dark  px-3 rounded-md hover:bg-primary  cursor-pointer  text-white ">Edit</button>
-            <button className="delete bg-primary-dark px-3 rounded-md hover:bg-primary cursor-pointer  text-white ">Delete</button>
+            <button className="edit font-bold  bg-primary-dark  py-1  px-3 rounded-md hover:bg-primary  cursor-pointer  text-white ">Edit</button>
+            <button className="delete font-bold  bg-primary-dark py-1  px-3 rounded-md hover:bg-primary cursor-pointer  text-white ">Delete</button>
 
           </div>
         </div>
