77897c47ea7e93304ce4f6cf99a31b85e57d7f0c
Suman Pun Magar <mannmagar19@gmail.com>
2026-03-23 17:13:54 +0545

    Day 38 added edit delete and showfinished features in todo app

--- ToDoList-App-usingReact/src/App.jsx
+++ ToDoList-App-usingReact/src/App.jsx
@@ -1,41 +1,58 @@
 import { useState } from "react";
-import { v4 as uuidv4 } from 'uuid';
+import { v4 as uuidv4 } from "uuid";
 import { useEffect } from "react";
 
 import Navbar from "./components/Navbar";
 import "./index.css";
 
 export default function App() {
-
-  
-  const [todo,settodo]= useState("")
+  const [todo, settodo] = useState("");
   // const [todos,settodos]= useState([])
   const [todos, settodos] = useState(() => {
     let saved = localStorage.getItem("todos");
     return saved ? JSON.parse(saved) : [];
   });
-  
-    useEffect(() => {
+
+const [showfinished, setshowfinished] = useState(false)
+
+
+  useEffect(() => {
     localStorage.setItem("todos", JSON.stringify(todos));
   }, [todos]);
 
+  const handleEdit = (e) => {
+    let todoToEdit = todos.filter((item)=>{
+      return item.id===e.target.id;
+    })
+    // console.log(todoToEdit)
+    settodo(todoToEdit[0].todo)
+   let id = e.target.id;
+    // console.log(id);
+    let newTodos = todos.filter((item) => {
+      return item.id !== id;
+    });
+    settodos(newTodos)
+  };
 
-  const handleEdit=()=>{
+  const handleDelete = (e) => {
+    confirm("Do you want to delete this todo..")
+    let id = e.target.id;
+    // console.log(id);
+    let newtodos = todos.filter((item) => {
+      return item.id !== id;
+    });
 
-  }
+    settodos(newtodos);
+  };
+  const handleChange = (e) => {
+    settodo(e.target.value);
+  };
 
-  const handleDelete=()=>{
-
-  }
-  const handleChange=(e)=>{
-    settodo(e.target.value)
-  }
-  
-  const handleAdd=()=>{
-    settodos([...todos, {id: uuidv4() ,todo, isCompleted:false}])
-    settodo("")
-  }
-  console.log(todos)
+  const handleAdd = () => {
+    settodos([...todos, { id: uuidv4(), todo, isCompleted: false }]);
+    settodo("");
+  };
+  // console.log(todos);
   // const handleCheckbox = (e) => {
   //   let id =e.target.id;
   //   let index = todos.findIndex(item=>{
@@ -45,50 +62,87 @@ export default function App() {
   //   newtodos[index].isCompleted=!newtodos[index].isCompleted;
   // }
   const handleCheckbox = (e) => {
-  let id = e.target.id;
+    let id = e.target.id;
 
-  let newtodos = todos.map((item) => {
-    if (item.id == id) {
-      return { ...item, isCompleted: !item.isCompleted };
-    }
-    return item;
-  });
+    let newtodos = todos.map((item) => {
+      if (item.id == id) {
+        return { ...item, isCompleted: !item.isCompleted };
+      }
+      return item;
+    });
 
-  settodos(newtodos);
-};
+    settodos(newtodos);
+  };
+
+  const togglefinished =(e)=>{
+setshowfinished(!showfinished) 
+  }
 
   return (
     <>
-        <Navbar />
-      <div className="main bg-violet-200  mb-5 p-5 rounded-xl mx-auto min-h-[80vh] ">
+      <Navbar />
+      <div className="main bg-violet-200  mb-5 p-5 rounded-xl mx-auto min-h-[80vh] w-3/4">
         <div className="addtodo my-5  text-black  ">
-
           <h1 className="font-bold text-3xl">Add New Todo</h1>
 
-          <input onChange={handleChange} value={todo} type="text" className="w-2/3 h-10  bg-bg rounded border-0  " />
+          <input
+            onChange={handleChange}
+            value={todo}
+            type="text"
+            className="w-2/3 h-10  bg-bg rounded border-0  "
+          />
 
-          <button onClick={handleAdd} className=" font-bold bg-primary-dark py-1  px-3 rounded-md hover:bg-primary cursor-pointer  text-white ml-5 " >Add</button>
+          <button
+            onClick={handleAdd} disabled={todo.length<5}
+            className=" font-bold bg-primary-dark py-1  px-3 rounded-md hover:bg-primary cursor-pointer  text-white ml-5 disabled:bg-warning "
+          >
+            Add
+          </button>
         </div>
-    
+
         <h2 className="bg text-black font-bold text-2xl ">Your Todos</h2>
+<input type="checkbox" onChange={togglefinished} checked={showfinished} /> Show Finished
         <div className="todos">
-          {todos.map((items)=>{
+          {todos.length===0 && <div className="m-4 font-bold text-3xl text-danger">No Todos to display</div>}
+          {todos.map((items) => {
+            return (showfinished || !items.isCompleted) && (
+              <div
+                className="todo py-2 flex gap-10 w-2/4 justify-between "
+                key={items.todo}
+              >
+                <input
+                  checked={items.isCompleted}
+                  onChange={handleCheckbox}
+                  type="checkbox"
+                  className="w-6 "
+                  value={items.isCompleted}
+                  id={items.id}
+                />
 
-            return <div className="todo py-2 flex gap-10 w-2/4 justify-between " key={items.todo}>
-
-<input checked={items.isCompleted} onChange={handleCheckbox} type="checkbox" className="w-6 " value={items.isCompleted} id={items.id} />
-            
-            
-            
-            <div className={`${items.isCompleted ? "bg-success":"bg-danger"} font-bold rounded-md p-1 text-white   `}>{items.todo}</div> 
-            {/* this is how to add the conditons and normal css classes we need to usethe template  strings  */}
-            <div className="btns flex gap-3 ">
-
-            <button onClick={handleEdit} className="edit font-bold  bg-primary-dark  py-1  px-3 rounded-md hover:bg-primary  cursor-pointer  text-white ">Edit</button>
-            <button onClick={handleDelete} className="delete font-bold  bg-primary-dark py-1  px-3 rounded-md hover:bg-primary cursor-pointer  text-white ">Delete</button>
-            </div>
-
-          </div>
+                <div
+                  className={`${items.isCompleted ? "bg-success" : "bg-danger"} font-bold rounded-md p-1 text-white   `}
+                >
+                  {items.todo}
+                </div>
+                {/* this is how to add the conditons and normal css classes we need to usethe template  strings  */}
+                <div className="btns flex gap-3 ">
+                  <button
+                    onClick={handleEdit}
+                    className="edit font-bold  bg-primary-dark  py-1  px-3 rounded-md hover:bg-primary  cursor-pointer  text-white "
+                    id={items.id}
+                  >
+                    Edit
+                  </button>
+                  <button
+                    onClick={handleDelete}
+                    className="delete font-bold  bg-primary-dark py-1  px-3 rounded-md hover:bg-primary cursor-pointer  text-white "
+                    id={items.id}
+                  >
+                    Delete
+                  </button>
+                </div>
+              </div>
+            );
           })}
         </div>
       </div>
