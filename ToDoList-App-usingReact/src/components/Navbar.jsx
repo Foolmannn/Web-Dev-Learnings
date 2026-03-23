82e131f34ae09beba84c5189bea933950d1b3197
Suman Pun Magar <mannmagar19@gmail.com>
2026-03-23 22:15:45 +0545

    Day 38 On todo app added the logo and fixed the navlinks

--- ToDoList-App-usingReact/src/components/Navbar.jsx
+++ ToDoList-App-usingReact/src/components/Navbar.jsx
@@ -21,15 +21,20 @@
 // export default Navbar
 
 import { Link, NavLink } from "react-router-dom";
+import logoImg from "../assets/todoo.png";
+
 
 function Navbar() {
   return (
-    <nav className="flex gap-6 p-4 bg-violet-500 text-white font-bold">
+    <nav className="flex gap-6 pb-0 p-4 bg-violet-500 text-3xl text-white font-bold  justify-between ">
+      <div className="logo  mx-30 "><img src={logoImg} alt="" srcset="" width={75}/></div>
+      <div className="links flex gap-15 mr-50 ">
+
       <NavLink
         to="/"
         className={({ isActive }) =>
           isActive ? "text-yellow-300" : ""
-        }
+      }
       >
         Home
       </NavLink>
@@ -38,10 +43,11 @@ function Navbar() {
         to="/tasks"
         className={({ isActive }) =>
           isActive ? "text-yellow-300" : ""
-        }
+      }
       >
         Tasks
       </NavLink>
+        </div>
     </nav>
   );
 }
